ac7e78644b3a33ca4b42c033e733a6736a9f6912
Adrian WEAVER <aweaver@student.42.fr>
2022-09-09 10:32:58 +0200

    FIX NORMINETTE:
    Squashed commit of the following:
    
    commit 0646b6028dda0e0cf1edeaff09a62366914f1246
    Author: Adrian WEAVER <aweaver@student.42.fr>
    Date:   Fri Sep 9 10:06:01 2022 +0200
    
        FIX NORMINETTE FORKS.C:
    
        Fixing norminette in sources/forks.c
    
        Changes to be committed:
        	modified:   sources/forks.c

--- philo/sources/forks.c
+++ philo/sources/forks.c
@@ -6,7 +6,7 @@
 /*   By: aweaver <aweaver@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/09 09:08:20 by aweaver           #+#    #+#             */
-/*   Updated: 2022/09/09 09:09:12 by aweaver          ###   ########.fr       */
+/*   Updated: 2022/09/09 10:05:11 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,37 +42,50 @@ int	ft_check_fork(t_list *list)
 	return (0);
 }
 
+static int	ft_pick_up_a_fork(int fork, t_philo *philo, t_list *list)
+{
+	if (fork == 0)
+	{
+		while (fork == 0)
+		{
+			if (philo->philo_nb % 2 == 0)
+				fork += ft_check_fork(list);
+			else
+				fork += ft_check_fork(list->next);
+		}
+		return (fork);
+	}
+	if (fork == 1)
+	{
+		while (fork == 1)
+		{
+			if (philo->philo_nb % 2 == 0)
+				fork += ft_check_fork(list->next);
+			else
+				fork += ft_check_fork(list);
+		}
+		return (fork);
+	}
+	return (fork);
+}
+
 int	ft_take_forks(t_list	*list)
 {
 	t_philo	*philo;
-	int		first_fork;
-	int		second_fork;
+	int		fork_nb;
 
-	first_fork = 0;
-	second_fork = 0;
+	fork_nb = 0;
 	philo = (t_philo *)list->content;
-	while (first_fork == 0)
-	{
-		if (philo->philo_nb % 2 == 0)
-			first_fork = ft_check_fork(list);
-		else
-			first_fork = ft_check_fork(list->next);
-	}
-	if (ft_get_current_time(philo->data, &philo->current_time) == 1)
-		return (1);
+	fork_nb = ft_pick_up_a_fork(fork_nb, philo, list);
+	if (ft_get_current_time(philo->data, &philo->current_time) == -1)
+		return (-1);
 	ft_print_msg(philo, "has taken a fork");
-	while (second_fork == 0)
-	{
-		if (philo->philo_nb % 2 == 0)
-			second_fork = ft_check_fork(list->next);
-		else
-			second_fork = ft_check_fork(list);
-	}
-	if (ft_get_current_time(philo->data, &philo->current_time) == 1)
-		return (1);
+	fork_nb = ft_pick_up_a_fork(fork_nb, philo, list);
+	if (ft_get_current_time(philo->data, &philo->current_time) == -1)
+		return (-1);
 	ft_print_msg(philo, "has taken a fork");
 	ft_eat(philo);
 	ft_release_forks(list);
 	ft_sleep(philo);
-	return (-1);
+	return (0);
 }
